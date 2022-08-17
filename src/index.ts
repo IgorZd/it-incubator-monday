e62f7d2d450be6845cf2b2d6a4951bd239fef42c
IgorZd <i.zdanevich@cthings.co>
2022-08-17 19:17:35 +0300

    add: change message

--- src/index.ts
+++ src/index.ts
@@ -3,7 +3,7 @@ const app = express();
 const port = process.env.PORT || 3000;
 
 app.get("/", (req: Request, res: Response) => {
-  res.send("Igor");
+  res.send("Igor123");
 });
 
 app.listen(port, () => {
