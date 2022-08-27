12fde351ee0f98322f28aaca2bd233ec69d91570
IgorZd <i.zdanevich@cthings.co>
2022-08-27 22:48:12 +0300

    fix: types

--- src/routes/posts-router.ts
+++ src/routes/posts-router.ts
@@ -16,7 +16,7 @@ const generateValidation = (fieldName: string, name: string) => {
       fieldName === "title"
         ? { min: 1, max: 30 }
         : fieldName === "shortDescription"
-        ? { min: 1, max: 99 }
+        ? { min: 1, max: 100 }
         : fieldName === "content"
         ? { min: 1, max: 1000 }
         : { min: 1 }
@@ -86,7 +86,7 @@ postsRouter.post(
         title: req.body.title,
         shortDescription: req.body.shortDescription,
         content: req.body.content,
-        bloggerId: +req.body.bloggerId,
+        bloggerId: req.body.bloggerId,
         bloggerName: bloggers[indexOfBlogger].name,
       };
       const newPost = postsRepository.createPosts(data);
@@ -96,7 +96,7 @@ postsRouter.post(
 );
 
 postsRouter.get("/:postId", (req: Request, res: Response) => {
-  const id = +req.params.postId;
+  const id = req.params.postId;
   const post = postsRepository.getPostById(id);
   const posts = postsRepository.findPosts();
 
@@ -114,9 +114,9 @@ postsRouter.put(
   ...validations,
   inputValidationMiddleware,
   (req: Request, res: Response) => {
-    const id = +req.params.id;
+    const id = req.params.id;
     const bloggers = bloggersRepository.findBloggers();
-    const bloggerInfo = getInfoAboutBlogger(+req.body.bloggerId, bloggers);
+    const bloggerInfo = getInfoAboutBlogger(req.body.bloggerId, bloggers);
     const { isBloggerExist, indexOfBlogger } = bloggerInfo;
     const isPostUpdated = postsRepository.updatePost(id);
     const isAuthorized = req.get("Authorization");
@@ -151,7 +151,7 @@ postsRouter.put(
         post.title = req.body.title;
         post.shortDescription = req.body.shortDescription;
         post.content = req.body.content;
-        post.bloggerId = +req.body.bloggerId;
+        post.bloggerId = req.body.bloggerId;
         post.bloggerName = bloggers[indexOfBlogger].name;
         res.status(204).send(post);
       }
@@ -162,7 +162,7 @@ postsRouter.put(
 );
 
 postsRouter.delete("/:id", (req: Request, res: Response) => {
-  const id = +req.params.id;
+  const id = req.params.id;
 
   const posts = postsRepository.findPosts();
   const isPostExist = posts.find((item: PostType) => item.id === id);
