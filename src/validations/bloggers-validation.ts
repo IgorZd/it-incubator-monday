17a4bae5e1cc5aad98534f613007a06dde004b48
IgorZd <i.zdanevich@cthings.co>
2022-08-29 20:54:58 +0300

    hotfix

--- src/validations/bloggers-validation.ts
+++ src/validations/bloggers-validation.ts
@@ -1,19 +1,15 @@
 import { body } from "express-validator";
 
 export const nameValidation = body("name")
-  .exists()
-  .withMessage("Name is empty")
   .trim()
   .isLength({ min: 1, max: 15 })
   .withMessage("Name should consist from 1 to 15 symbols");
 
 export const youtubeUrlValidation = body("youtubeUrl")
-  .exists()
-  .withMessage("YoutubeUrl is empty")
   .trim()
+  .isLength({ min: 1, max: 100 })
+  .withMessage("YoutubeUrl should consist from 1 to 100 symbols")
   .matches(
     /^https:\/\/([a-zA-Z0-9_-]+\.)+[a-zA-Z0-9_-]+(\/[a-zA-Z0-9_-]+)*\/?$/
   )
-  .withMessage("Incorrect format of url")
-  .isLength({ min: 1, max: 100 })
-  .withMessage("YoutubeUrl should consist from 1 to 100 symbols");
+  .withMessage("Incorrect format of url");
